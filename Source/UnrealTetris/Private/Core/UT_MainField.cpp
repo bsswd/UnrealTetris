66fe3445c20e7fbefafa0ecd85d87d6ca8c02a45
Alex <112611942+bsswd@users.noreply.github.com>
2026-04-05 14:10:14 +0700

    Initial commit

---
+++ Source/UnrealTetris/Private/Core/UT_MainField.cpp
@@ -0,0 +1,21 @@
+﻿// Unreal Tetris Game. Made by Alex Sinkin. (c)
+
+#include "UnrealTetris/Public/Core/UT_MainField.h"
+
+
+AUT_MainField::AUT_MainField()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AUT_MainField::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AUT_MainField::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/UnrealTetris/Public/Core/UT_MainField.h
@@ -0,0 +1,26 @@
+﻿// Unreal Tetris Game. Made by Alex Sinkin. (c)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Camera/CameraComponent.h"
+#include "UT_MainField.generated.h"
+
+UCLASS()
+class UNREALTETRIS_API AUT_MainField : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	
+	AUT_MainField();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual void Tick(float DeltaTime) override;
+};
